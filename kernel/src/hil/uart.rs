66f7794e078f024499e397cf6e0b4fce010f9b08
Branden Ghena <brghena@umich.edu>
2016-09-23 12:33:48 -0400

    Redesign of the UART HIL

--- kernel/src/hil/uart.rs
+++ kernel/src/hil/uart.rs
@@ -1,42 +1,84 @@
+/// UART hardware interface
+
 #[derive(Copy, Clone)]
-pub enum Parity {
-    Even = 0,
-    Odd = 1,
-    ForceZero = 2,
-    ForceOne = 3,
-    None = 4,
-    Multidrop = 6,
+pub enum StopBits {
+    StopBits_1 = 0,
+    StopBits_2 = 2,
 }
 
 #[derive(Copy, Clone)]
-pub enum Mode {
-    Normal = 0,
-    FlowControl = 2,
+pub enum Parity {
+    None = 0,
+    Odd = 1,
+    Even = 2,
 }
 
 #[derive(Copy, Clone)]
 pub struct UARTParams {
-    // Parity and stop bits should both be enums.
-    pub baud_rate: u32,
-    pub data_bits: u8,
+    pub baud_rate: u32, // baud rate in bit/s
+    pub stop_bits: StopBits,
     pub parity: Parity,
-    pub mode: Mode,
+    pub hw_flow_control: bool,
+}
+
+/// The type of error encountered during UART transaction
+#[derive(Copy, Clone)]
+pub struct Error {
+    /// Parity error during receive
+    ParityError,
+
+    /// Framing error during receive
+    FramingError,
+
+    /// Overrun error during receive
+    OverrunError,
+
+    /// UART Receive not ready
+    RXNotReady,
+
+    /// UART Transmit not ready
+    TXNotReady,
+
+    /// No error occurred and the command completed successfully
+    CommandComplete,
 }
 
 pub trait UART {
+    /// Initialize UART
+    ///
+    /// # Panics
+    ///
+    /// if UARTParams are invalid for the current chip
     fn init(&mut self, params: UARTParams);
-    fn send_byte(&self, byte: u8);
-    fn send_bytes(&self, bytes: &'static mut [u8], len: usize);
-    fn read_byte(&self) -> u8;
-    fn rx_ready(&self) -> bool;
-    fn tx_ready(&self) -> bool;
-    fn enable_rx(&self);
-    fn disable_rx(&mut self);
-    fn enable_tx(&self);
-    fn disable_tx(&mut self);
+
+    /// Transmit data
+    //XXX: change this to Amit's EitherBuffer
+    fn transmit(&self, tx_data: &'static [u8], tx_len: usize);
+
+    /// Receive data until buffer is full
+    fn receive_buffer(&self, rx_buffer: &'static mut [u8]);
+
+    /// Receive data until `timeout` bit periods have passed since the last byte
+    /// or buffer is full. Does not timeout until at least one byte has been
+    /// received
+    ///
+    /// * `timeout` - number of bit periods since last data received
+    fn receive_until_finished(&self, rx_buffer: &'static mut [u8], timeout: u8);
+
+    /// Receive data until `terminator` data byte has been received or buffer
+    /// is full
+    ///
+    /// * `terminator` - data byte terminating a reception
+    fn receive_until_terminator(&self, rx_buffer: &'static mut [u8], terminator: u8);
 }
 
+
+/// Implement Client to receive callbacks from UART
 pub trait Client {
-    fn read_done(&self, byte: u8);
-    fn write_done(&self, buffer: &'static mut [u8]);
+    /// UART transmit complete
+    fn transmit_complete(&self, tx_buffer: &'static [u8], error: Error);
+
+    /// UART receive complete
+    fn receive_complete(&self, rx_buffer: &'static mut [u8], rx_len: u8, error: Error);
 }
+
